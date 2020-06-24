50e807907cbf428a5d2f5c85163006f16361f39d
Joshua Patterson <jleegippies@gmail.com>
2020-06-23 20:30:34 -0500

    Drew graphics on the screen

--- src/Runner.java
+++ src/Runner.java
@@ -1,10 +1,18 @@
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Graphics;
 
-public class Runner {
+public class Runner extends JPanel {
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
+        JFrame frame = new JFrame("Anthill Java");
+        frame.getContentPane().add(new Runner());
         frame.setSize(1280, 720);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+        frame.setResizable(false);
+    }
+
+    public void paint(Graphics g) {
+        g.drawLine(10, 10, 1280, 720);
     }
 }
